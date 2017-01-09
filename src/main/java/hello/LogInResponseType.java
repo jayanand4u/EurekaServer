981ab822cadca602d925a4375950aee2adc1a0ba
Jayanand Jha <JJha@gogoair.com>
2017-01-08 19:36:31 -0600

    adding the content to the project

---
+++ src/main/java/hello/LogInResponseType.java
@@ -0,0 +1,108 @@
+package hello;
+
+public class LogInResponseType {
+
+    protected String vendorId;
+    protected String transactionId;
+    protected String sessionId;
+    protected String confirmationCode;
+    
+    /**
+     * Gets the value of the vendorId property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getVendorId() {
+        return vendorId;
+    }
+
+    /**
+     * Sets the value of the vendorId property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setVendorId(String value) {
+        this.vendorId = value;
+    }
+
+    /**
+     * Gets the value of the transactionId property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getTransactionId() {
+        return transactionId;
+    }
+
+    /**
+     * Sets the value of the transactionId property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setTransactionId(String value) {
+        this.transactionId = value;
+    }
+
+    /**
+     * Gets the value of the sessionId property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getSessionId() {
+        return sessionId;
+    }
+
+    /**
+     * Sets the value of the sessionId property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setSessionId(String value) {
+        this.sessionId = value;
+    }
+
+
+
+    /**
+     * Gets the value of the confirmationCode property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getConfirmationCode() {
+        return confirmationCode;
+    }
+
+    /**
+     * Sets the value of the confirmationCode property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setConfirmationCode(String value) {
+        this.confirmationCode = value;
+    }
+
+}
